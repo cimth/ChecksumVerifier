4b860c76e3a28b9265d9a4fc3f539d822f34353e
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2021-02-02 13:36:18 +0100

    Enable creating standalone JAR for Linux, Windows and Mac via Maven package

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
      *==             START APPLICATION                ==*
      *==================================================*/
 
-    public static void main(String[] args) {
+    public static void launch(String[] args) {
         launch();
     }
 
